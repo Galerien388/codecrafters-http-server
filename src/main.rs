1e4b0033d9a323ecf916bca8553d5172addc0437
joel van eyken <j_van_eyken@hotmail.com>
2026-04-12 12:13:46 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -31,17 +31,37 @@ fn main() -> Result<()> {
 fn handle_connections(stream: Result<TcpStream, Error>) -> Result<()> {
     let mut stream = stream?;
 
-    // reader borrow the stream
-    // this way the reader will be dropped
-    // before stream is borrow as mut in write_all method
-    // Note: is not necessary with new Rust NLL (non lexical lifetimes)
-    let request = {
-        let mut reader = BufReader::new(&stream);
-        Request::read_from(&mut reader)?
-    };
+    loop {
+        let request = {
+            let mut reader = BufReader::new(&stream);
+            Request::read_from(&mut reader)?
+        };
 
-    dbg!(&request);
-    let response = routes::router(&request)?;
-    stream.write_all(&response.to_http_bytes()?)?;
+        dbg!(&request);
+        let response = routes::router(&request)?;
+        stream.write_all(&response.to_http_bytes()?)?;
+
+        if request.get_header_value("connection").unwrap_or("") == "close" {
+            break;
+        }
+    }
     Ok(())
 }
+
+// fn handle_connections(stream: Result<TcpStream, Error>) -> Result<()> {
+//     let mut stream = stream?;
+//
+//     // reader borrow the stream
+//     // this way the reader will be dropped
+//     // before stream is borrow as mut in write_all method
+//     // Note: is not necessary with new Rust NLL (non lexical lifetimes)
+//     let request = {
+//         let mut reader = BufReader::new(&stream);
+//         Request::read_from(&mut reader)?
+//     };
+//
+//     dbg!(&request);
+//     let response = routes::router(&request)?;
+//     stream.write_all(&response.to_http_bytes()?)?;
+//     Ok(())
+// }
